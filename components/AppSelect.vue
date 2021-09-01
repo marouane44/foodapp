cf8d7eb5bc00a5221dde6ccef5322dd31756a0d7
marwanhaz43 <marwanhaz43@gmail.com>
2021-09-01 22:13:11 +0100

    second_commet_dropdown

---
+++ components/AppSelect.vue
@@ -0,0 +1,35 @@
+ <template>
+     <div>
+ <label :for="select">what cuisine do you want {{select}}</label>
+
+<select @change="$emit('change', $event.target.value)" :id="select" :name="select">
+     <option value>---pleas select a {{select}}---</option>
+  <option v-for="option in selectOptions" :key ="option" :value="option">
+    
+      {{ option }}
+
+ </option>
+</select> 
+     </div>
+ </template>
+ 
+ <script>
+     export default {
+          props:{
+              select:{
+                type: String,
+                default:  "cuisine"
+              }, 
+              selectOptions:{
+
+                  type:Array,
+                  default: () => ["tacos","pizza","dim sum"],
+              }
+          },
+
+     }
+ </script>
+ 
+ <style lang="scss" scoped>
+ 
+ </style>
--- pages/restaurants.vue
+++ pages/restaurants.vue
@@ -1,7 +1,11 @@
 <template>
   <main class="container restaurant">
+    
+  
     <div class="restaurantheading">
       <h1>Restaurants</h1>
+      <AppSelect @change="selectedRestaurant = $event"/>
+      <pre>{{$data}}</pre>
     </div>
 
     <AppRestaurantInfo />
@@ -10,10 +14,16 @@
 
 <script>
 import AppRestaurantInfo from "@/components/AppRestaurantInfo.vue";
-
+import AppSelect from '@/components/AppSelect.vue';
 export default {
   components: {
+    AppSelect,
     AppRestaurantInfo
+  },
+  data(){
+    return {
+      selectedRestaurant: "",
+    };
   }
 };
 </script>
